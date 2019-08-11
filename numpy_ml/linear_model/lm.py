9b07f89de8c2ed95ef55f55cd13ab15312938ca5
superhzf <superhzf@hotmail.com>
2019-08-11 12:19:20 -0700

    basic frame

---
+++ numpy_ml/linear_model/lm.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+
+
+def sigmoid(z):
+    a = 1/(1+np.exp(-1*z))
+    return a
+
+
+def logloss(y_true,y_hat):
+    loss = -1*np.sum(y_true*np.log(y_hat) +(1-y_true)*np.log(1-y_hat))
+    return loss
+
+
+class LogisticRegression:
+    def __init__(self):
+        self.W = None
+        self.b = None
+
+    def fit(self, x, y):
+        m, n = x.shape
+        self.b = np.random.rand(1,m)
+        self.W = np.random.rand(1,n)
+        z = np.dot(self.W,np.transpose(x))
+        a = sigmoid(z)
+        loss = logloss(y,a)
+        dz = a - y
+        dw = (1/m)*x*
+
+
+train = pd.DataFrame({"var1":[1,2,3],'var2':[4,5,6],'y':[1,0,1]})
+lr = LogisticRegression()
+lr.fit(train[['var1','var2']].values,train['y'].values)
+print (lr.W,lr.b)
+print (np.shape(lr.W))
