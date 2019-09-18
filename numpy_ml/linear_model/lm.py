80a99aea7b3c9b134dae986fab4e02b651c848ee
zach_hu <zach.hu@earnest.com>
2019-09-17 22:14:56 -0700

    add fit

--- numpy_ml/linear_model/lm.py
+++ numpy_ml/linear_model/lm.py
@@ -1,14 +1,15 @@
 import numpy as np
-import pandas as pd
+from sklearn.datasets import load_iris
 
 
 def sigmoid(z):
-    a = 1/(1+np.exp(-1*z))
+    neg_z = np.negative(z)
+    a = 1/(1+np.exp(neg_z))
     return a
 
 
 def logloss(y_true,y_hat):
-    loss = -1*np.mean(y_true*np.log(y_hat)+(1-y_true)*np.log(1-y_hat))
+    loss = np.mean(-1*(y_true*np.log(y_hat)+(1+np.negative(y_true))*np.log(1+np.negative(y_hat))))
     return loss
 
 
@@ -16,21 +17,31 @@ class LogisticRegression:
     def __init__(self):
         self.W = None
         self.b = None
+        self.loss = []
+        self.training_loss = None
 
-    def fit(self, x, y):
+    def fit(self, X, Y, alpha, round):
         # n features, m samples
-        n, m = x.shape
-        self.b = np.random.rand(1,m)
-        self.W = np.random.rand(1,n)
-        z = np.dot(self.W,np.transpose(x))
-        a = sigmoid(z)
-        loss = logloss(y,a)
-        dz = a - y
-        dw =
+        n, m = X.shape
+        self.b = np.random.rand(1,1)
+        self.W = np.random.rand(n,1)
+        for i in range(round):
+            Z = np.matmul(np.transpose(self.W),X)+self.b
+            A = sigmoid(Z)
+            dZ = A-Y
+            dW = np.matmul(X,np.transpose(dZ))/m
+            db = np.sum(dZ)/m
+            self.W = self.W - np.multiply(dW, alpha)
+            self.b = self.b - np.multiply(db, alpha)
+            round_n_loss = logloss(Y, A)
+            self.loss.append(round_n_loss)
+        self.training_loss = round_n_loss
 
+data = load_iris()
+X = np.transpose(data['data'])
+Y = np.reshape(data['target'],(1,-1))
 
-train = pd.DataFrame({"var1":[1,2,3],'var2':[4,5,6],'y':[1,0,1]})
 lr = LogisticRegression()
-lr.fit(train[['var1','var2']].values,train['y'].values)
-print (lr.W,lr.b)
-print (np.shape(lr.W))
+lr.fit(X,Y,0.01,100)
+
+print (lr.training_loss)
