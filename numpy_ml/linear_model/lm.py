a2fb90defbf89f74f79c617758caa63a3457cbee
zach_hu <zach.hu@earnest.com>
2019-11-01 07:49:19 -0700

    update file

--- numpy_ml/linear_model/lm.py
+++ numpy_ml/linear_model/lm.py
@@ -22,8 +22,8 @@ class LogisticRegression:
     def fit(self, X, Y, alpha, round, normalize = False):
         # n features, m samples
         n, m = X.shape
-        self.b = np.random.rand(1,1)
-        self.W = np.random.rand(n,1)
+        self.b = np.random.rand(1, 1)
+        self.W = np.random.rand(n, 1)
         if normalize:
             mean = np.mean(X,axis=1)
             std = np.std(X,axis=1)
@@ -33,13 +33,14 @@ class LogisticRegression:
             Z = np.matmul(np.transpose(self.W),X)+self.b
             A = sigmoid(Z)
             dZ = A-Y
-            dW = np.matmul(X,np.transpose(dZ))/m
+            dW = np.matmul(X, np.transpose(dZ))/m
             db = np.sum(dZ)/m
             self.W = self.W - dW*alpha
             self.b = self.b - db*alpha
             round_n_loss = logloss(Y, A)
             self.loss.append(round_n_loss)
         self.training_loss = round_n_loss
+
 
 data = load_breast_cancer()
 X = np.transpose(data['data'])
