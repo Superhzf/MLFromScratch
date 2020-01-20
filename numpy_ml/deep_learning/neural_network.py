428816ac93c48f211a7eba039b42530ff1baff3b
superhzf <superhzf@hotmail.com>
2020-01-20 09:12:36 -0800

    add init file

--- numpy_ml/deep_learning/neural_network.py
+++ numpy_ml/deep_learning/neural_network.py
@@ -1,5 +1,6 @@
 import numpy as np
 import progressbar
+from 
 
 class NeuralNetwork():
     """
@@ -47,3 +48,32 @@ class NeuralNetwork():
         # If the layer has weights that needs to be initialized
         if hasattr(layer,'initialize'):
             layer.initialize(optimizer=self.optimizer)
+
+        # Add layer to the network
+        self.layers.append(layer)
+
+    def test_on_batch(self,X,y):
+        """Evaluates the model over a single batch of samples"""
+        y_pred = self._forward_pass(X,training=False)
+        loss = np.mean(self.loss_function.loss(y,y_pred))
+        acc = self.loss_function.acc(y,y_pred)
+
+        return loss, acc
+
+
+    def train_on_batch(self,X,y):
+        """Single gradient update over one batch of samples"""
+        y_pred = self._forward_pass(X)
+        loss = np.mean(self.loss_function.loss(y,y_pred))
+        acc = self.loss_function.acc(y,y_pred)
+        # Calculate the gradient of the loss function w.r.t y_pred
+        loss_grad = self.loss_function.gradient(y,y_pred)
+        # Backpropagate. Update weights
+        self._backward_pass(loss_grad = loss_grad)
+
+
+    def fit(self,X,y,n_epochs,batch_size):
+        """Train the model for a fixed number of epochs"""
+        for _ in self.progressbar(range(n_epochs)):
+            batch_error = []
+            for X_batch,y_batch in batch
