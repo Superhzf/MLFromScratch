31ea23592186fb4111ea886db362ef1fd962ee9f
superhzf <superhzf@hotmail.com>
2020-01-22 11:28:26 -0800

    add dropout

--- numpy_ml/deep_learning/layers.py
+++ numpy_ml/deep_learning/layers.py
@@ -165,3 +165,34 @@ class BatchNormalization(Layer):
 
     def out_shape(self):
         return self.input_shape
+
+class Dropout(Layer):
+    """
+    A layer that randomly sets a fraction p of the output units of the previous
+    layer to zero.
+
+    Parameters:
+    --------------------------
+    p: float
+        The probability that unit x is set to zero.
+    """
+    def __init__(self,p = 0.2):
+        self.p = p
+        self._mask = None
+        self.input_shape = None
+        self.n_units = None
+        self.pass_through = True
+        self.trainable = True
+
+    def forward_pass(self,X,training=True):
+        c = (1-self.p)
+        if training:
+            self._mask = np.random.uniform(size = X.shape) > self.p
+            c = self._mask
+        return X * c
+
+    def backward_pass(self,accum_grad):
+        return accum_grad*self._mask
+
+    def output_shape(self):
+        return self.input_shape
