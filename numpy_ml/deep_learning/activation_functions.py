9a605da69cf6a996680bc7ea5f5b58d1af0700cc
superhzf <superhzf@hotmail.com>
2020-01-23 15:48:40 -0800

    update activation functions

--- numpy_ml/deep_learning/activation_functions.py
+++ numpy_ml/deep_learning/activation_functions.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# Why sigmoid function instead of anything else?
+# TODO
 class Sigmoid():
     def __call__(self,x):
         return 1/(1+np.exp(-x))
@@ -8,8 +10,8 @@ class Sigmoid():
         p = self.__call__(x)
         return p*(1-p)
 
-# The reason to use softmax instead of standard normalization for multi-class
-# classification is that 
+# Why Softmax instead of standard normalization ?
+# TODO
 class Softmax():
     def __call__(self,x):
         exp_x = np.exp(x-np.max(x,axis=-1,keepdims=True))
