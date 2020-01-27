8afe7b2a563ee6f12baefff4b2b17aab0f3e2ea2
superhzf <superhzf@hotmail.com>
2020-01-27 13:30:53 -0800

    update optimizers

--- numpy_ml/deep_learning/optimizers.py
+++ numpy_ml/deep_learning/optimizers.py
@@ -1,5 +1,24 @@
 import numpy as np
 
+# Stochastic Gradient Descent with momentum
 class StochasticGradientDescent():
     def __init__(self,learning_rate = 0.01,momentum=0.01):
-        
+        self.learning_rate = learning_rate
+        self.momentum = momentum
+        self.w_update = None
+
+    def update(self,w,grad_wrt_w):
+        # if not initialized
+        if self.w_update is None:
+            self.w_update = np.zeros(np.shape(w))
+
+        # Use momentum if set
+        self.w_update = self.momentum * self.w_update + (1-self.momentum) * grad_wrt_w
+        # Move against the gradient to minimize loss
+        return w - self.learning_rate*self.w_update
+
+# why RMSprop?
+# Answer: 
+class RMSprop():
+    def __init__(self,learning_rate = 0.01,rho = 0.9):
+        self.learning_rate = learning_rate
