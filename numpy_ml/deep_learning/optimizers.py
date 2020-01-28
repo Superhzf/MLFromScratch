2aaf86d8cdbe4a5340936fc8d34b4f92be92a13d
Zefu Hu <zefuhu@Zefus-MacBook-Pro.local>
2020-01-27 16:02:49 -0800

    add adagrad

--- numpy_ml/deep_learning/optimizers.py
+++ numpy_ml/deep_learning/optimizers.py
@@ -37,3 +37,19 @@ class RMSprop():
 
         self.Eg = self.rho*self.Eg + (1-self.rho)*np.power(grad_wrt_w,2)
         return w - self.learning_rate*grad_wrt_w/np.sqrt(self.Eg+self.eps)
+
+
+class Adagrad():
+    def __init__(self,learning_rate=0.01):
+        self.learning_rate = learning_rate
+        self.G = None # sum of squares of the gradients
+        self.eps = 1e-8
+
+    def update(self,w,grad_wrt_w):
+        # If not initialized
+        if self.G is None:
+            self.G = np.zeros(np.shape(w))
+        # Add the square of the gradient of the loss function at w
+        self.G += np.power(grad_wrt_w,2)
+        # Adaptive gradient with higher learning_rate for sparse data
+        return w-self.learning_rate*grad_wrt_w/np.sqrt(self.G+self.eps)
