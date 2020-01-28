a4fa52441c9dddf3925123c5488826b2545ee0f4
superhzf <superhzf@hotmail.com>
2020-01-27 18:56:12 -0800

    add adam

--- numpy_ml/deep_learning/optimizers.py
+++ numpy_ml/deep_learning/optimizers.py
@@ -72,3 +72,31 @@ class Adagrad():
         # If the feature is missing, then self.G becomes relatively smaller
         # and the step is relatively larger
         return w-self.learning_rate*grad_wrt_w/np.sqrt(self.G+self.eps)
+
+# Adam is the combination of SGD with momentum and RMSprop
+# 
+class Adam():
+    def __init__(self,learning_rate=0.001,b1=0.9,b2=0.999):
+        self.learning_rate = learning_rate
+        self.eps = 1e-8
+        self.m = None
+        self.v = None
+        # Decay rates
+        self.b1 = b1
+        self.b2 = b2
+
+    del update(self,w,grad_wrt_w):
+        # if not initialized
+        if self.m is None:
+            self.m = np.zeros(np.shape(grad_wrt_w))
+            self.v = np.zeros(np.shape(grad_wrt_w))
+
+        self.m = self.b1*self.m+(1-self.b1)*grad_wrt_w
+        self.v = self.b2*self.v+(1-self.b2)*np.power(grad_wrt_w,2)
+
+        m_hat = self.m/(1-self.b1)
+        v_hat = self.v/(1-self.b2)
+
+        self.update = self.learning_rate*m_hat/(np.sqrt(v_hat)+self.eps)
+
+        return w - self.update
