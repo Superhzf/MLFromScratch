0264c67c13727db06bb53fa63f2ffc418bbee0e1
superhzf <superhzf@hotmail.com>
2020-01-29 09:34:20 -0800

    type: feat
    add divide_on_feature function to data_manipulation script. The idea is 
    to divide a dataset based on a threshold at feature_i for a random 
    forest

--- numpy_ml/utils/data_manipulation.py
+++ numpy_ml/utils/data_manipulation.py
@@ -10,3 +10,18 @@ def batch_generator(X,y=None,batch_size = 64):
             yield X[begin:end],y[begin:end]
         else:
             yield X[begin:end]
+
+def divide_on_feature(X,feature_i,threshold):
+    """
+    Divide dataset X based on if sample value on feature_i is larger than
+    the given threshold
+    """
+    split_func = None
+    if isinstance(threshold,int) or isinstance(threshold,float):
+        X_1 = X[X[:,feature_i]>=threshold]
+        X_2 = X[X[:,feature_i]<threshold]
+    else:
+        X_1 = X[X[:,feature_i]==threshold]
+        X_2 = X[X[:,feature_i]!=threshold]
+
+    return np.array([X_1,X_2])
