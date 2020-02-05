7cfe521c3adc963d3520aad40df71564194d80ef
superhzf <superhzf@hotmail.com>
2020-02-05 09:14:10 -0800

    type: feat
    Add train test split class

--- numpy_ml/utils/data_manipulation.py
+++ numpy_ml/utils/data_manipulation.py
@@ -36,3 +36,15 @@ def to_categorical(x,n_col=None):
     one_hot = np.zeros((x.shape[0], n_col))
     one_hot[np.arange(x.shape[0]), x] = 1
     return one_hot
+
+def train_test_split(X, y, test_size=0.5, shuffle=True, seed=None):
+    """ Split the data into train and test sets """
+    if shuffle:
+        X, y = shuffle_data(X, y, seed)
+    # Split the training data from test data in the ratio specified in
+    # test_size
+    split_i = len(y) - int(len(y) // (1 / test_size))
+    X_train, X_test = X[:split_i], X[split_i:]
+    y_train, y_test = y[:split_i], y[split_i:]
+
+    return X_train, X_test, y_train, y_test
