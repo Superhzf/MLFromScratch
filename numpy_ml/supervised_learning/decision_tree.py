805ddc4bc6c4ca833639fd59e5c77d0f20dafb32
superhzf <superhzf@hotmail.com>
2020-02-04 18:19:17 -0800

    type: feat
    Add XGBoost regression tree class

--- numpy_ml/supervised_learning/decision_tree.py
+++ numpy_ml/supervised_learning/decision_tree.py
@@ -228,3 +228,47 @@ class ClassificationTree(DecisionTree):
         self._impurity_calculation = _calculate_information_gain
         self._leaf_value_calculation = _majority_vote
         super(ClassificationTree,self).fit(X,y)
+
+class XGBoostRegressionTree(DecisionTree):
+    """
+    Regression tree for XGBoost
+    """
+    #
+    def _split(self,y):
+        """
+        The first half of y is y_true, the second half of y is y_pred
+        """
+        col = int(np.shape(y)[1]/2)
+        y,y_pred = y[:,:col],y[:,col:]
+        return y,y_pred
+
+    def _gain(self,y,y_pred):
+        # ref: https://xgboost.readthedocs.io/en/latest/tutorials/model.html
+        numerator = np.power(self.loss.gradient(y,y_pred).sum(),2)
+        denominator = self.loss.hess(y,y_pred).sum()
+        return 0.5*(numerator/denominator)
+
+    def _gain_by_taylor(self,y,y1,y2):
+        # Split
+        y,y_pred = self._split(y)
+        y1,y1_pred = self._split(y1)
+        y2,y2_pred = self._split(y2)
+
+        true_gain = self._gain(y1,y1_pred)
+        false_gain = self._gain(y2,y2_pred)
+        gain = self._gain(y,y_pred)
+        return true_gain+false_gain-gain
+
+    def _approximate_update(self,y):
+        # y split into y,y_pred
+        y,y_pred = self._split(y)
+        # Newton's method
+        # ref: https://xgboost.readthedocs.io/en/latest/tutorials/model.html
+        gradient = np.sum(self.loss.gradient(y,y_pred),axis=0)
+        hess = np.sum(self.loss.hess(y,y_pred),axis=0)
+        update_approximation = gradient/hessian
+        return update_approximation
+
+    def fit(self,X,y):
+        self._impurity_calculation = self._gain_by_taylor
+        self._leaf_value_calculation = _approximate_update
