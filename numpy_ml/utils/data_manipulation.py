3139eaa75bec687f4a25b93c15c4cfe7eb166683
superhzf <superhzf@hotmail.com>
2020-02-08 10:26:15 -0800

    type: feat
    Add class to normalize input data

--- numpy_ml/utils/data_manipulation.py
+++ numpy_ml/utils/data_manipulation.py
@@ -67,3 +67,10 @@ def polynomial_features(X,degree):
         X_new[:,i] = np.prod(X[:,index_combs],axis=1)
 
     return X_new
+
+
+def normalize(X, axis=-1, order=2):
+    """ Normalize the dataset X """
+    l2 = np.atleast_1d(np.linalg.norm(X, order, axis))
+    # l2[l2 == 0] = 1???
+    return X / np.expand_dims(l2, axis)
