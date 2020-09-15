344b52d273f148285bacce02d7006c5de8e71954
Superhzf <superhzf@hotmail.com>
2020-09-15 15:18:22 +0800

    type:feat
    Add unit test for leakyRelu function and change the formula to calculate 
    gradient

--- unit_tests/tests.py
+++ unit_tests/tests.py
@@ -7,7 +7,7 @@ from numpy_ml.deep_learning.loss_functions import BinaryCrossEntropy
 from numpy.testing import assert_almost_equal
 import torch.nn as nn
 import torch
-from numpy_ml.deep_learning.activation_functions import Sigmoid, Softmax, ReLU
+from numpy_ml.deep_learning.activation_functions import Sigmoid, Softmax, ReLU, LeakyReLU
 
 
 def test_binary_cross_entropy(cases):
@@ -56,24 +56,29 @@ def test_sigmoid_activation(cases):
 
     N = int(cases)
 
-    mine = Sigmoid()
-    gold = nn.Sigmoid()
-
     i = 0
     while i < N:
         n_dims = np.random.randint(1, 100)
         z = random_tensor((1, n_dims))
         z_tensor = torch.tensor(z,requires_grad=True)
+        z_tensor.retain_grad()
 
-        gold_output = gold(z_tensor)
-        my_output = mine(z)
-        # sum up the output value of sigmoid as the loss function
-        loss = torch.sum(gold_output)
-        loss.backward()
+        mine = Sigmoid()
+        gold = nn.Sigmoid()
+
+        gold_value = gold(z_tensor)
+        gold_value.retain_grad()
+
+        loss_tensor = torch.square(gold_value).sum()/2.
+        loss_tensor.backward()
+
+        gold_grad = z_tensor.grad
+        mine_value = mine(z)
+
         # compare forward
-        assert_almost_equal(my_output, gold_output.detach().numpy())
+        assert_almost_equal(mine_value, gold_value.detach().numpy())
         # compare backward
-        assert_almost_equal(mine.gradient(z), z_tensor.grad)
+        assert_almost_equal(mine.gradient(z)*gold_value.grad.numpy(), gold_grad)
         i += 1
     print ('Successfully testing Sigmoid function!')
 
@@ -98,34 +103,69 @@ def test_softmax_activation(cases):
         mine_value = mine(z)
         # compare forward value
         assert_almost_equal(mine_value, gold_value.detach().numpy())
-        # compare backward value
-        assert_almost_equal(mine.gradient(z), z_tensor.grad)
         i += 1
 
-    print ('Successfully testing Softmax function!')
+    print ('Successfully testing Softmax function (forward only)!')
 
 def test_relu_activation(cases):
 
     np.random.seed(12345)
 
     N = int(cases)
-
-    mine = ReLU()
-    gold = nn.ReLU()
-
     i = 0
     while i < N:
-        n_dims = np.random.randint(1, 100)
-        z = random_stochastic_matrix(1, n_dims)
-        z_tensor = torch.tensor(z,requires_grad=True)
 
-        loss_tensor = torch.sum(z_tensor)
-        loss_tensor.backward()
+        n_dims = np.random.randint(1, 100)
+        z = np.random.randn(1, n_dims)
+        z_tensor = torch.tensor(z,requires_grad=True)
+        z_tensor.retain_grad()
+
+        mine = ReLU()
+        gold = nn.ReLU()
+
         gold_value = gold(z_tensor)
+        gold_value.retain_grad()
+
+        # Let the 1/2 times the sume of squares as the loss function
+        loss_tensor = torch.square(gold_value).sum()/2.
+        loss_tensor.backward()
+
+        gold_grad = z_tensor.grad
         mine_value = mine(z)
         # compare forward value
         assert_almost_equal(mine_value, gold_value.detach().numpy())
         # compare backward value
-        assert_almost_equal(mine.gradient(z), z_tensor.grad)
+        assert_almost_equal(mine.gradient(z)*gold_value.grad.numpy(), gold_grad)
         i += 1
     print ('Successfully testing ReLU function!')
+
+def test_leakyrelu_activation(cases):
+
+    N = int(cases)
+
+    i = 0
+    while i < N:
+        n_dims = np.random.randint(1, 100)
+        z = np.random.randn(1, n_dims)
+        z_tensor = torch.tensor(z,requires_grad=True)
+        z_tensor.retain_grad()
+
+        alpha = np.random.uniform(0, 10)
+
+        mine = LeakyReLU(alpha=alpha)
+        gold = nn.LeakyReLU(negative_slope=alpha)
+
+        gold_value = gold(z_tensor)
+        gold_value.retain_grad()
+        loss_tensor = torch.square(gold_value).sum()/2.
+        loss_tensor.backward()
+        gold_grad = z_tensor.grad
+        mine_value = mine(z)
+
+        # compare forward value
+        assert_almost_equal(mine_value, gold_value.detach().numpy())
+        # compare backward value
+        assert_almost_equal(mine.gradient(z), gold_grad/gold_value.grad)
+        i += 1
+
+    print ('Successfully testing LeakyReLU function!')
