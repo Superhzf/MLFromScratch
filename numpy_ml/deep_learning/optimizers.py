50f03133f7d0818a50809b5b984993021c3b991b
Superhzf <superhzf@hotmail.com>
2020-09-18 09:23:34 +0800

    type: feat
    Add unit test for adadelta optimizer

--- numpy_ml/deep_learning/optimizers.py
+++ numpy_ml/deep_learning/optimizers.py
@@ -78,7 +78,7 @@ class Adagrad():
 class Adadelta():
     def __init__(self, rho=0.95, eps=1e-6):
         self.E_w_updt = None
-        self.E_grad = None
+        self.E_avg = None
         self.w_updt = None
         self.eps = eps
         self.rho = rho
@@ -87,12 +87,12 @@ class Adadelta():
         if self.w_updt is None:
             self.w_updt = np.zeros(np.shape(w))
             self.E_w_updt = np.zeros(np.shape(w))
-            self.E_grad = np.zeros(np.shape(grad_wrt_w))
+            self.E_avg = np.zeros(np.shape(grad_wrt_w))
 
-        self.E_grad = self.rho * self.E_grad + (1 - self.rho) * np.power(grad_wrt_w, 2)
+        self.E_avg = self.rho * self.E_avg + (1 - self.rho) * np.power(grad_wrt_w, 2)
 
         RMS_delta_w = np.sqrt(self.E_w_updt + self.eps)
-        RMS_grad = np.sqrt(self.E_grad + self.eps)
+        RMS_grad = np.sqrt(self.E_avg + self.eps)
 
         adaptive_lr = RMS_delta_w / RMS_grad
         self.w_updt = adaptive_lr * grad_wrt_w
--- unit_tests/conftest.py
+++ unit_tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+# define how many test cases will running, the default number is 15
 def pytest_addoption(parser):
     parser.addoption(
         "--cases", action="store", default="15", help="The number of random test cases"
