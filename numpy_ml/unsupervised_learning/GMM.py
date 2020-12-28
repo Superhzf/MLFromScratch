35670b4b496dfee23edc4262b9f4372b5b90e09c
Superhzf <superhzf@hotmail.com>
2020-12-28 15:48:00 +0800

    type: refactor
    Refactor the GMM algorithm making it use the inverse of the covariance 
    matrix instead of the covariance matrix to improve efficiency

--- numpy_ml/unsupervised_learning/GMM.py
+++ numpy_ml/unsupervised_learning/GMM.py
@@ -2,7 +2,12 @@ import numpy as np
 from numpy.testing import assert_allclose
 
 class GMM(object):
-    def __init__(self, C:int=3, seed:int=None) -> None:
+    def __init__(self,
+                 C: int=3,
+                 seed: int=None,
+                 weights_init: np.ndarray=None,
+                 means_init: np.ndarray=None,
+                 precisions_init: np.ndarray=None) -> None:
         """
         A Gaussian maxiture model trained via the expectation maimization
         algorithm
@@ -15,7 +20,16 @@ class GMM(object):
             The number of mixture components in the GMM.
         seed: int
             Seed for the random number generator.
-
+        weights_init: np.ndarray
+            The user-provided initial weights. If None, weights are
+            initialized in the _initialize_parameters() function.
+        means_init: np.ndarray
+            The user-provided initial means. If None, weights are
+            initialized in the _initialize_parameters() function.
+        precisions_init: np.ndarray
+            The user-provided initial precisions (inverse of the covariance matrices).
+            If it is None, precisions are initialized in the _initialize_parameters() function.
+            It makes prediction more convenient.
         Attributes:
         ----------------------
         N: int
@@ -24,34 +38,54 @@ class GMM(object):
             The dimension of each observation in the training set.
         pi: numpy.array of shape (C,)
             The cluster priors. pi[j] shows the probability that the latent
-            variable comes from the Gaussian distribution j.
+            variable comes from the Gaussian distribution j. pi is also called
+            weights.
         Q: numpy.array of shape of shape (N, C)
             The distribution over the laten variable Z.
         mu: numpy.array of shape (C, d)
             Means of Gaussian distributions
         sigma: numpy.array of shape (C, d, d)
-            Covariance matrices for Gaussian distributions
+            Covariance matrices for Gaussian distributions.
+        inverse_sigma: numpy.array of shape (C, d, d)
+            The inverse of the Covariance matrices for Gaussian distributions.
+            Storing this makes prediction convenient.
         """
         self.C = C
         self.N = N
         self.d = None
         self.seed = seed
+        self.weights_init = weights_init
+        self.means_init = means_init
+        self.precisions_init = precisions_init
+        self.is_converged = None
 
         if self.seed:
             np.random.seed(self.seed)
 
         def _initialize_parameters(self) -> None:
-            self.pi = np.random.rand(self.C)
-            self.pi = self.pi/self.pi.sum()
+            if self.weights_init is None:
+                self.pi = np.random.rand(self.C)
+                self.pi = self.pi/self.pi.sum()
+            else:
+                self.pi = self.weights_init
 
             self.Q = np.zeros([self.N, self.C])
+
             # Initialize mu by randomly select C values from each dimension
-            self.mu = np.zeros([self.C, self.d])
-            for this_dim in range(self.d):
-                this_mu = np.random.choice(self.X[:,this_dim],self.C)
-                self.mu[:, this_dim] = this_mu
+            if self.means_init is None:
+                self.mu = np.zeros([self.C, self.d])
+                for this_dim in range(self.d):
+                    this_mu = np.random.choice(self.X[:,this_dim],self.C)
+                    self.mu[:, this_dim] = this_mu
+            else:
+                self.mu = self.means_init
+
             # Diag covariance matrices
-            self.sigma = np.array([np.identity(d) for _ in range(C)])
+            if self.precisions_init is None:
+                self.sigma = np.array([np.identity(d) for _ in range(C)])
+            else:
+                self.inverse_sigma = self.precisions_init
+                self.sigma = np.zeros(self.inverse_sigma.shape)
 
             self.best_pi = None
             self.best_mu = None
@@ -70,15 +104,15 @@ class GMM(object):
                     pi_c = self.pi[this_c]
                     z_c = self.Q[i, this_c]
                     mu_c = self.mu[this_c, :]
-                    sigma_c = self.sigma[this_c, :, :]
+                    inverse_sigma_c = self.inverse_sigma[this_c, :, :]
 
                     log_pi_c = np.log(pi_c+eps)
-                    log_p_x_i = log_gaussian_pdf(x_i, mu_c, sigma_c)
+                    log_p_x_i = log_gaussian_pdf(x_i, mu_c, inverse_sigma_c)
                     log_z_c = np.log(z_c+eps)
-                    elbo += z_c * (log_pi_c + log_p_x_i - log_z_c)
+                    elbo += (z_c * (log_pi_c + log_p_x_i - log_z_c))
             return elbo
 
-        def fit(self, X: , max_iter:int=100, tol:float=1e-3):
+        def fit(self, X: np.ndarray, max_iter:int=100, tol:float=1e-3) -> bool:
             """
             Fit the parameters of the GMM on the training set
 
@@ -96,7 +130,9 @@ class GMM(object):
 
             Returns:
             ---------------
-
+            converged: bool
+                True: the model is converged.
+                False: the model is not converged.
             """
             self.X = X
             self.N = X.shape[0]
@@ -107,7 +143,26 @@ class GMM(object):
 
             for _ in range(max_iter):
                 self._E_step()
-                self._M_step()
+                try:
+                    self._M_step()
+                except np.linalg.LinAlgError:
+                    print ('Cannot calculatet the inverse of the covariance matrix')
+                    self.is_converged = False
+                    return
+                this_elbo = self.likelihood_lower_bound()
+
+                is_converged = np.abs(this_elbo - prev_elbo) <= tol
+                if is_converged:
+                    self.is_converged = True
+                    return
+                prev_elbo = this_elbo
+                if this_elbo > self.best_elbo:
+                    self.best_elbo = this_elbo
+                    self.best_mu = self.mu
+                    self.best_pi = self.pi
+                    self.best_sigma = self.sigma
+            self.is_converged = True
+            return
 
         def _E_step(self) -> None:
             eps = np.finfo(float).eps
@@ -118,10 +173,10 @@ class GMM(object):
                 for this_c in range(self.C):
                     pi_c = self.pi[this_c]
                     mu_c = self.mu[this_c, :]
-                    sigma_c = self.sigma[this_c, :, :]
+                    inverse_sigma_c = self.inverse_sigma[this_c, :, :]
 
                     log_pi_c = np.log(pi_c+eps)
-                    log_p_x_i = log_gaussian_pdf(x_i, mu_c, sigma_c)
+                    log_p_x_i = log_gaussian_pdf(x_i, mu_c, inverse_sigma_c)
                     denom.append(log_pi_c+log_p_x_i)
 
             # logsumexp: logF1, logF2, logF3, ... -> log(F1 + F2 + F3 + ...)
@@ -133,20 +188,20 @@ class GMM(object):
 
         def _M_step(self) -> None:
             # total weights over all observations for each Gaussian distribution
-            total_weights = np.sum(self.Q, axis=0)
+            total_imp = np.sum(self.Q, axis=0)
 
             # update priors
-            self.pi = np.sum(self.Q, axis=0)/self.N
+            self.pi = total_imp/self.N
 
             # update Gaussian distribution means
             mu_numer = [np.dot(self.Q[:, this_c], X) for this_c in range(C)]
-            for idx, (this_mu_numer, this_weights) in enumerate(zip(mu_numer, total_weights)):
+            for idx, (this_mu_numer, this_weights) in enumerate(zip(mu_numer, total_imp)):
                 self.mu[idx,:] = this_mu_numer/this_weights
 
             # update Gaussian distribution covariance
             for this_c in range(self.C):
                 mu_c = self.mu[this_c,:]
-                weight_c = total_weights[this_c]
+                weight_c = total_imp[this_c]
 
                 this_sigma = np.zeros((self.d, self.d))
                 for i in range(self.N):
@@ -157,7 +212,30 @@ class GMM(object):
                 this_sigma = this_sigma/weight_c if weight_c>0 else this_sigma
                 self.sigma[c, :, :] = this_sigma
 
+            # update the inverse of the covariance matrices for future interations
+            self.inverse_sigma = np.linalg.inv(self.sigma)
+
             assert_allclose(np.sum(self.pi), 1)
 
-def log_gaussian_pdf(x_i: np.ndarray, mu: float, sigma: float) -> float:
-    pass
+def log_gaussian_pdf(x_i: np.ndarray, mu: float, inverse_sigma: float) -> float:
+    """
+    Compute log N(x_i | mu, sigma)
+    """
+    n = len(mu)
+    a = n * np.log(2 * np.pi)
+    _, b = np.linalg.slogdet(inverse_sigma)
+    b = 1/b
+
+    # calculate the inverse of sigma
+    y = inverse_sigma @ (x_i - mu)
+    c = np.dot(x_i - mu, y)
+    return -0.5 * (a + b + c)
+
+def logsumexp(log_probs, axis=None) -> float:
+    """
+    logF1, logF2, logF3, ... -> log(F1 + F2 + F3 + ...)
+    """
+    _max = np.max(log_probs)
+    ds = log_probs - _max
+    exp_sum = np.exp(ds).sum(axis=axis)
+    return _max + np.log(exp_sum)
