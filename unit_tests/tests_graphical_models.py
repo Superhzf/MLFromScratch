44c6491bdf3126908f896c74d091052b211ae112
Superhzf <superhzf@hotmail.com>
2021-01-06 11:11:19 +0800

    type: feat
    Add unit test for the decode function in HMM

--- unit_tests/tests_graphical_models.py
+++ unit_tests/tests_graphical_models.py
@@ -90,3 +90,52 @@ def test_DiscreteHMM_posteriors(cases: str) -> None:
         i+=1
 
     print('Successfully testing the posterior function in HMM!')
+
+def test_DiscreteHMM_decode(cases: str) -> None:
+    np.random.seed(12346)
+    cases = int(cases)
+    i = 1
+    N_decimal = 4
+    while i < cases:
+        tol=1e-3
+        n_samples = np.random.randint(10, 50)
+        hidden_states = np.random.randint(3, 6)
+        # symbols is the number of unqiue observation types.
+        symbols = np.random.randint(4, 9)
+        X = []
+        lengths = []
+        for _ in range(n_samples):
+            # the actual length is seq_length + 1
+            seq_length = symbols
+            this_x = np.random.choice(range(symbols), size=seq_length, replace=False)
+            X.append(this_x)
+            lengths.append(seq_length)
+        max_iter = 100
+
+
+        hmm_gold = MultinomialHMM(n_components=hidden_states, n_iter=100, tol=tol)
+        X_gold = np.concatenate(X).reshape((-1,1))
+        hmm_gold.fit(X_gold, lengths)
+        gold_A = hmm_gold.transmat_
+        gold_B = hmm_gold.emissionprob_
+        gold_pi = hmm_gold.startprob_
+        gold_logprob, gold_state_sequence = hmm_gold.decode(X_gold, lengths)
+        hmm_mine = DiscreteHMM(hidden_states=hidden_states,
+                               symbols=symbols,
+                               A=gold_A,
+                               B=gold_B,
+                               pi=gold_pi)
+        mine_logprob_list = []
+        mine_state_sequence = []
+        for this_x in X:
+            this_mine_logprob, this_mine_state_sequence = hmm_mine.decode(this_x)
+            mine_logprob_list.append(this_mine_logprob)
+            mine_state_sequence.append(this_mine_state_sequence)
+        mine_state_sequence = np.concatenate(mine_state_sequence)
+        mine_logprob = sum(mine_logprob_list)
+        assert_almost_equal(mine_logprob, gold_logprob, decimal=N_decimal)
+        assert_almost_equal(mine_state_sequence, gold_state_sequence, decimal=N_decimal)
+        i+=1
+    print('Successfully testing the function of computing decodes!')
+
+        
