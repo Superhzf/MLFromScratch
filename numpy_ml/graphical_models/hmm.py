120d63bd1e3bae7b5b0e8b6b8734c7e68567482c
Superhzf <superhzf@hotmail.com>
2021-01-14 12:12:08 +0800

    type: feat
    Initialize Gaussian HMM

--- numpy_ml/graphical_models/hmm.py
+++ numpy_ml/graphical_models/hmm.py
@@ -29,13 +29,13 @@ class DiscreteHMM:
             parameter for a HMM model.
         symbols: int
             The number of unique observation types.
-        A: numpy.ndarray of shape (N, N)
+        A: numpy.ndarray of shape (hidden_states, hidden_states)
             The transmission matrix between hidden states. For example, A[i, j]
             gives the probability from state i to state j.
-        B: numpy.ndarray of shape (N, V)
+        B: numpy.ndarray of shape (hidden_states, symbols)
             The emission matrix. For example, B[i, j] gives the probability to
             observe j given the state i.
-        pi: numpy.ndarray of shape (N, )
+        pi: numpy.ndarray of shape (hidden_states, )
             The prior probability of hidden states.
         seed: int
             The seed used to randomly generate A and B is not provided.
@@ -423,6 +423,126 @@ class DiscreteHMM:
         return beta
 
 
+class GaussianHMM:
+    def __init__(self,
+                 hidden_states:int=1,
+                 A:np.ndarray=None,
+                 n_features:int=None,
+                 means: np.ndarray=None,
+                 covar:np.ndarray=None,
+                 pi:np.ndarray=None,
+                 seed:int=None,
+                 tol:float=1e-3,
+                 max_iter:int=100)->None:
+        """
+        A Hidden Markov Model with Gaussian emission distributions. Specifically,
+        the input X is allowed to be continuous, although the hidden states are
+        still supposed to be discrete.
+
+        Parameters:
+        -------------------------------
+        hidden_states: int
+            The number of unique hidden states
+        A: numpy.ndarray of shape (hidden_states, hidden_states)
+            The transmission matrix between states. A[i,j] gives the probability
+            from state i to state j.
+        n_features: int
+            
+        means: numpy.ndarray of shape (hidden_states)
+
+
+        """
+        self.hidden_states=hidden_states
+        self.A=A
+        self.means=means
+        self.covar=covar
+        self.n_features=n_features
+        self.pi=pi
+        self.seed=seed
+        self.tol=tol
+        self.max_iter=max_iter
+
+    def _initialize(self) -> None:
+        "Initialize parameters and do parameter check"
+        if self.seed:
+            np.random.seed(self.seed)
+        # Initialize self.A
+        if self.A is None:
+            self.A = np.full((self.hidden_states, self.hidden_states),1/self.hidden_states)
+
+        # Initialize self.B
+        if self.B is None:
+            self.B = []
+            for _ in range(self.hidden_states):
+                this_B = np.random.dirichlet(np.ones(self.symbols),size=1)[0]
+                self.B.append(this_B)
+            self.B = np.array(self.B)
+
+        # Initialize self.pi
+        if self.pi is None:
+            self.pi = np.ones(self.hidden_states)
+            self.pi = self.pi/self.hidden_states
+
+    def log_likelihood(self, x: np.ndarray) -> float:
+        """
+        Given A, B, pi, and a set of observations, compute the probability of
+        observations.
+
+        The likelhood is calculated via the forward algorithm.
+
+        Parameters:
+        ----------------
+        x: numpy.ndarray of shape (T, ).
+            A single set of observations. Note that T is not the same for
+            different observations.
+
+        Returns:
+        ---------------
+        likelihood: float
+            The likelihood of the observation.
+        """
+        alpha_it = self._forward(x)
+        log_likelihood = logsumexp(alpha_it[:,-1])
+        return log_likelihood
+
+    def _forward(self, x:np.ndarray) -> np.ndarray:
+        """
+        Parameters:
+        ----------------
+        x: numpy.ndarray of shape (T, ).
+            A single set of observations. Note that T is not the same for
+            different observations.
+
+        Return:
+        ---------------
+        alpha_it: numpy.ndarray of shape (hidden_states, T)
+            alpha_it[i,t] gives logP(Zt=Si, X[1:t])
+        """
+        T = x.shape[0]
+        alpha_it = np.zeros((self.hidden_states, T))
+        # initialization
+        with np.errstate(divide="ignore"):
+            for this_s in range(self.hidden_states):
+                alpha_it[this_s,0] = np.log(self.pi) + log_gaussian_pdf(x[0],
+                                                                   self.means[this_s],
+                                                                   self.covar[this_s,:,:])
+
+        work_buffer = np.zeros(self.hidden_states)
+        for this_t in range(1, T):
+            this_obs = x[this_t]
+            for this_state in range(self.hidden_states):
+                for this_state_prev in range(self.hidden_states):
+                    with np.errstate(divide="ignore"):
+                        work_buffer[this_state_prev]=np.log(self.A[this_state_prev, this_state]) + \
+                                                     alpha_it[this_state_prev, this_t-1]
+                with np.errstate(divide="ignore"):
+                    alpha_it[this_state, this_t]=logsumexp(work_buffer) + \
+                                                log_gaussian_pdf(this_obs,
+                                                        self.means[this_state],
+                                                        self.covar[this_state,:,:])
+
+        return alpha_it
+
 def logsumexp(log_probs, axis=None):
     """
     Redefine scipy.special.logsumexp
@@ -432,3 +552,18 @@ def logsumexp(log_probs, axis=None):
     ds = log_probs - _max
     exp_sum = np.exp(ds).sum(axis=axis)
     return _max + np.log(exp_sum)
+
+def log_gaussian_pdf(x_i: np.ndarray, mu: np.ndarray, inverse_sigma: np.ndarray) -> float:
+    """
+    Compute log P(x_i | z_j; mu, sigma)
+    """
+    n = len(mu)
+    a = np.power(2 * np.pi, n)
+    b = np.linalg.det(inverse_sigma)
+    b = abs(1/b)
+    ab = np.log(a * b)
+
+    # calculate the inverse of sigma
+    y = inverse_sigma @ (x_i - mu)
+    c = np.dot(x_i - mu, y)
+    return -0.5 * (ab + c)
