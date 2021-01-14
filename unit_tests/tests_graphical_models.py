6df9b2029e3f187bdc0b3b9aaf85e6e4a74e1099
Superhzf <superhzf@hotmail.com>
2021-01-14 17:34:51 +0800

    type: feat
    Add unit test for computing posteriors in Gaussian HMM

--- unit_tests/tests_graphical_models.py
+++ unit_tests/tests_graphical_models.py
@@ -218,14 +218,11 @@ def test_GaussHMM_forward(cases: str) -> None:
     while i < cases:
         n_samples = np.random.randint(10, 50)
         hidden_states = np.random.randint(3, 6)
-        # symbols is the number of unqiue observation types.
         n_features = np.random.randint(4, 9)
         X = []
         lengths = []
         for _ in range(n_samples):
-            # the actual length is seq_length + 1
             seq_length = np.random.randint(4, 9)
-#             this_x = np.random.choice(range(symbols), size=seq_length, replace=False)
             this_x = np.random.rand(seq_length,n_features)
 
             X.append(this_x)
@@ -263,3 +260,55 @@ def test_GaussHMM_forward(cases: str) -> None:
         i+=1
 
     print('Successfully testing the forward algorithm in Gaussian HMM!')
+
+
+def test_GaussHMM_posterior(cases: str) -> None:
+    np.random.seed(12346)
+    cases = int(cases)
+    i = 1
+    N_decimal = 4
+    max_iter = 100
+    tol=1e-3
+    while i < cases:
+        n_samples = np.random.randint(10, 50)
+        hidden_states = np.random.randint(3, 6)
+        n_features = np.random.randint(4, 9)
+        X = []
+        lengths = []
+        for _ in range(n_samples):
+            seq_length = np.random.randint(4, 9)
+            this_x = np.random.rand(seq_length,n_features)
+
+            X.append(this_x)
+            lengths.append(seq_length)
+
+        hmm_gold = GaussianHMM(n_components=hidden_states,
+                               covariance_type='full',
+                               n_iter=max_iter,
+                               tol=tol)
+
+        X_gold = np.concatenate(X)
+        hmm_gold.fit(X_gold, lengths)
+
+        gold_means = hmm_gold.means_
+        gold_pi = hmm_gold.startprob_
+        gold_n_features = hmm_gold.n_features
+        gold_transmat = hmm_gold.transmat_
+        gold_means = hmm_gold.means_
+        gold_covars = hmm_gold.covars_
+
+        hmm_mine = GaussHMM(hidden_states=hidden_states,
+                               A=gold_transmat,
+                               n_features=gold_n_features,
+                               means=gold_means,
+                               covar=gold_covars,
+                               pi=gold_pi,
+                               tol=tol,
+                               max_iter=max_iter)
+        _,gold_posteriors = hmm_gold.score_samples(X_gold, lengths)
+        mine_posterior_list = [hmm_mine.posterior(this_x) for this_x in X]
+        mine_posterior_list = np.concatenate(mine_posterior_list)
+        assert_almost_equal(mine_posterior_list, gold_posteriors, decimal=N_decimal)
+        i+=1
+
+    print('Successfully testing the function of computing posteriors in Gaussian HMM!')
