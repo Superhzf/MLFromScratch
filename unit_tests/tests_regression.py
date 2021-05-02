72540f88c99d93fc5d3f5e6b80016263aeb7751c
Superhzf <superhzf@hotmail.com>
2021-05-02 10:57:30 +0800

    type: feat
    Add the unit test for the lasso regression with coordinate descent

--- unit_tests/tests_regression.py
+++ unit_tests/tests_regression.py
@@ -3,7 +3,8 @@ sys.path.append('..')
 import numpy as np
 from numpy.testing import assert_almost_equal
 from sklearn.linear_model import SGDRegressor
-from numpy_ml.supervised_learning.regression import RidgeRegression,LassoRegression
+from numpy_ml.supervised_learning.regression import RidgeRegression,LassoRegression, LassoRegressionCD
+from sklearn.linear_model import Lasso
 
 def test_ridge_regression_SGD(cases: str) -> None:
     np.random.seed(12346)
@@ -122,3 +123,50 @@ def test_lasso_regression_PGD(cases: str) -> None:
         i+=1
 
     print('Successfully testing Lasso Regression with Proxomal Gradient Descent!')
+
+
+def test_lasso_regression_CD(cases: str) -> None:
+
+    np.random.seed(123)
+    cases = int(cases)
+    i = 1
+    N_decimal = 4
+    max_iter = 100
+    tol=1e-3
+    while i < cases:
+        n_samples = np.random.randint(10, 50)
+        n_features = np.random.randint(5,10)
+        X = np.random.normal(size=(n_samples,n_features))
+        alpha = np.random.uniform(0.1,0.2)
+
+        coef_limit=1/np.sqrt(n_features)
+        coef = np.random.uniform(-coef_limit,coef_limit,(n_features,))
+        bias = np.random.uniform(-coef_limit,coef_limit,(1,))
+        y = X@coef+bias
+#         print ('coef',coef,'bias',bias,'y',y)
+
+        gold = Lasso(alpha=alpha,
+                     fit_intercept=True,
+                     tol=tol,
+                     max_iter=max_iter)
+        mine = LassoRegressionCD(alpha=alpha,max_iter=max_iter,tol=tol)
+        mine.fit(np.copy(X),np.copy(y))
+        gold.fit(np.copy(X),np.copy(y))
+        gold_weights = gold.coef_
+        gold_bias = gold.intercept_
+        gold_n_iter = gold.n_iter_
+        gold_dual_gap = gold.dual_gap_
+
+        mine_weights = mine.w
+        mine_bias = mine.b
+        mine_n_iter = mine.this_iter
+        mine_dual_gap = mine.dual_gap
+
+        assert_almost_equal(mine_weights, gold_weights, decimal=N_decimal)
+        assert_almost_equal(mine_bias, gold_bias, decimal=N_decimal)
+        assert_almost_equal(mine_n_iter, gold_n_iter, decimal=N_decimal)
+        assert_almost_equal(mine_dual_gap, gold_dual_gap, decimal=N_decimal)
+
+        i+=1
+
+    print('Successfully testing Lasso Regression with Coordinate Descent method!')
